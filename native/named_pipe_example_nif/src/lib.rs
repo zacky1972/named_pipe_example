d54aacfa04a804d4a31d137602dfd5c7acb711d8
Susumu Yamazaki <zacky1972@gmail.com>
2018-06-02 10:54:35 +0900

    create run

--- native/named_pipe_example_nif/src/lib.rs
+++ native/named_pipe_example_nif/src/lib.rs
@@ -15,13 +15,12 @@ mod atoms {
 
 rustler_export_nifs! {
     "Elixir.NamedPipeExampleNif",
-    [("add", 2, add)],
+    [("run", 1, run)],
     None
 }
 
-fn add<'a>(env: NifEnv<'a>, args: &[NifTerm<'a>]) -> NifResult<NifTerm<'a>> {
-    let num1: i64 = try!(args[0].decode());
-    let num2: i64 = try!(args[1].decode());
+fn run<'a>(env: NifEnv<'a>, args: &[NifTerm<'a>]) -> NifResult<NifTerm<'a>> {
+    let _message: String = try!(args[0].decode());
 
-    Ok((atoms::ok(), num1 + num2).encode(env))
+    Ok((atoms::ok()).encode(env))
 }
